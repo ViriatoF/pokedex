485160ac6b9b1d0aacac175df74c006197886c13
ViriatoF <viriato.ferreira44@gmail.com>
2024-10-22 12:27:17 +0200

    Function PokemonCard with ternaire done

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,27 @@
 function PokemonCard(){
 
+  const pokemon = pokemonList[0];
+
+
   return(
-  <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-    <figcaption>Bulbasaur</figcaption>
-  </figure>
+    <figure>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="" /> : <p>???</p>}
+      
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
 )
-}
+};
+
+
+const pokemonList =[
+  {
+    name: "Bulbasaur",
+    imgSrc : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 
 export default PokemonCard;
