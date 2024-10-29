2b531658ce24787b56f2ee946a7d095ab3ac5adc
ViriatoF <viriato.ferreira44@gmail.com>
2024-10-29 21:39:06 +0100

    Quest 7 done

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,37 @@ import "./App.css";
 import "./assets/card.css";
 import { useState } from "react";
 
+
 import PokemonCard from "./components/PokemonCard";
+import NavBar from "./components/NavBar";
 
 const pokemonList =[
   {
+    id : 1,
     name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    id:2,
     name: "charmander",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
   },
   {
+    id:3,
     name: "squirtle",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
   },
   {
+    id:4,
     name: "pikachu",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
+    id:5,
     name: "mew",
     imgSrc : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png"
   },
@@ -33,26 +40,20 @@ const pokemonList =[
 
 function App() {
 
-  const [pokemonIndex, SetPokemonIndex] = useState(0);
-  console.log(pokemonIndex);
-  
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
-  const handleClickNext = () => {
-    SetPokemonIndex(pokemonIndex+1 );
-    console.log("button +");
+  const handleClickChangePoke = (name: string): void => {
+    const pokemonId = pokemonList.findIndex((el) => el.name === name);
+    setPokemonIndex(pokemonId);
   }
-  const handleClickReturn = () => {
-    SetPokemonIndex(pokemonIndex-1 );
-    console.log("button -");
-    
-  }
+  
+  
 
   
   return (
     <>
-      {pokemonIndex >0 ? <button onClick={handleClickReturn}> Précédent</button> : ""}
-      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
-      {pokemonIndex < pokemonList.length - 1? <button onClick={handleClickNext}> Suivant</button> : ""}
+      <NavBar pokemonList={pokemonList} handleClickChangePoke={handleClickChangePoke} />
+      <PokemonCard Pokemon={pokemonList[pokemonIndex]}/>
     </>
   );
 }
